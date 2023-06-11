267a4a2e23df622741b41b5e75d6375f4bdadea7
Lucas <lucas@MacBook-Pro-de-Lucas.local>
2023-06-11 14:53:00 -0300

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,39 +38,41 @@ export default function Home() {
   const handleSelectedNumber = (number: string) => {
     setSelectedNumber(number);
   };
-  
+
   return (
     <main className="flex min-h-screen flex-col items-center px-2 md:px-5 py-10 md:p-10">
-      <Image src="/perfil.png" alt="Eifol" width={180} height={180} className="w-36"/>
-      <Image
-        className="py-4"
-        src="/eifol.png"
-        alt="Eifol"
-        width={80}
-        height={80}
-      />
-
-      <p className="text-center text-sm max-w-xl md:text-lg">
-        Olá, Somos Lucas & Alane e iremos para o EIFOL no mês de Julho. Queremos
-        que você nos ajude a estar presente naquele lugar!!! Contamos com a
-        sua sua ajuda.
-      </p>
-      <p className="text-lg pt-4 max-w-xl">
-        <b>Valor: R$ 10,00</b> | <b>Prêmio: R$ 100,00</b>
-      </p>
-      <p className="text-base pt-4 max-w-xl">PIX: 83 989050001</p>
+      <div className="md:flex items-center gap-4">
+        <Image
+          src="/perfil.png"
+          alt="Eifol"
+          width={180}
+          height={180}
+          className="m-auto mb-4"
+        />
+        <div className="flex flex-col items-center max-w-lg">
+          <p className="text-center text-sm max-w-xl md:text-lg">
+            Olá, Somos Lucas & Alane e iremos para o EIFOL no mês de Julho.
+            Queremos que você nos ajude a estar presente naquele lugar!!!
+            Contamos com a sua sua ajuda.
+          </p>
+          <p className="text-lg pt-4 max-w-xl">
+            <b>Valor: R$ 10,00</b> | <b>Prêmio: R$ 100,00</b>
+          </p>
+          <p className="text-base pt-4 max-w-xl">PIX: 83 989050001</p>
+        </div>
+      </div>
       <p className="text-base pt-4 max-w-xl">
-        O sorteio será no instagram: @lucaslimasz_
+        O sorteio será 02/07 no instagram: @lucaslimasz_
       </p>
       <section className="my-10 bg-white border-2 border-[#174580]">
         <div className="flex justify-center flex-wrap max-w-4xl text-slate-950">
           {data.map((item) => {
-            const alredyBought = numbers.includes(item.number)
+            const alredyBought = numbers.includes(item.number);
             return (
               <button
                 className={`flex-1 w-10 flex items-center justify-center 
                 p-1 border md:p-2 md:border-2 border-[#174580]
-                ${alredyBought && 'text-xl'}
+                ${alredyBought && "text-xl"}
               `}
                 key={item.number}
                 onClick={() => handleSelectedNumber(item.number)}
